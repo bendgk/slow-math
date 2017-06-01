5674080a2426ef08e57be44440bd5f9053737859
Benjamin Kosten <benkosten@gmail.com>
2017-05-31 22:33:14 -0400

    Add very slow math

--- pyncil.py
+++ pyncil.py
@@ -5,13 +5,36 @@ Might be slow, but hopefully more powerful?
 """
 
 class SlowMath:
-    def modernize(self, x, y):
+    def standardize(self, x, y):
         while len(x) != len(y):
             if len(x) > len(y): y = "0" + y
             else: x = "0" + x
 
         return (x, y)
 
+    def add(self, pair):
+        x = pair[0][::-1]
+        y = pair[1][::-1]
+
+        remainder = 0
+        out = ""
+        
+        for i in range(len(x)):
+            b = str(int(x[i]) + int(y[i]) + remainder)
+            if len(b) == 2:
+                remainder = 1
+                b = b[1]
+            else: remainder = 0
+
+            out = b + out
+
+        return out
+
 s = SlowMath()
 
-print(s.modernize("1237", "38"))
+num = "23738295234897589034759278"
+
+for i in range(10000000):
+   num = s.add((num, num))
+
+print(num)
